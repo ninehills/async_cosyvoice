4206d4d3784b33429ec4814849181234be52c364
Tao Yang <swulling@gmail.com>
2025-07-14 16:29:10 +0800

    限制speech_tokenizer的CPU op threads 数量避免容器里报错

--- frontend.py
+++ frontend.py
@@ -191,7 +191,7 @@ class CosyVoiceFrontEnd:
         self.device = 'cpu'
         option = onnxruntime.SessionOptions()
         option.graph_optimization_level = onnxruntime.GraphOptimizationLevel.ORT_ENABLE_ALL
-        # option.intra_op_num_threads = 1
+        option.intra_op_num_threads = 8
         self.campplus_session = onnxruntime.InferenceSession(campplus_model, sess_options=option, providers=["CUDAExecutionProvider" if torch.cuda.is_available() else
                                                                                 "CPUExecutionProvider"])
         #self.speech_tokenizer_session = onnxruntime.InferenceSession(speech_tokenizer_model, sess_options=option,
